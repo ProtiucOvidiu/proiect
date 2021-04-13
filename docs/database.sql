4379892d451833f78332c62ce21f6670f2128780
Diana Anton <diana.anton00@e-uvt.ro>
2021-04-13 18:08:49 +0300

    Updated DB structure

--- docs/database.sql
+++ docs/database.sql
@@ -1,25 +1,52 @@
 CREATE TABLE `users` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(255) unique not null,
-  `password` varchar(255) not null,
-  `full_name` varchar(255) not null,
-  `email` varchar(255) unique not null,
-  `phone_number` varchar(10) not null,
-  `group_id` int not null,
-  `extra_permissions` int
+  `username` varchar(255),
+  `password` varchar(255),
+  `full_name` varchar(255),
+  `email` varchar(255),
+  `phone_number` number(10)
 );
 
 CREATE TABLE `groups` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) not null,
-  `string_permissions` varchar(255) not null,
-  `description` varchar(255) 
+  `name` varchar(255),
+  `description` varchar(255)
 );
 
 CREATE TABLE `permissions` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) not null,
+  `name` varchar(255),
   `description` varchar(255)
 );
 
-ALTER TABLE `users` ADD FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`);
+CREATE TABLE `user_groups_relation` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int,
+  `group_id_1` int,
+  `group_id_2` int,
+  `group_id_3` int
+);
+
+CREATE TABLE `groups_perm_relation` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `group_id` int,
+  `perm_id_1` int,
+  `perm_id_2` int,
+  `perm_id_3` int
+);
+
+ALTER TABLE `user_groups_relation` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `groups_perm_relation` ADD FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`);
+
+ALTER TABLE `user_groups_relation` ADD FOREIGN KEY (`group_id_1`) REFERENCES `groups` (`id`);
+
+ALTER TABLE `user_groups_relation` ADD FOREIGN KEY (`group_id_2`) REFERENCES `groups` (`id`);
+
+ALTER TABLE `user_groups_relation` ADD FOREIGN KEY (`group_id_3`) REFERENCES `groups` (`id`);
+
+ALTER TABLE `groups_perm_relation` ADD FOREIGN KEY (`perm_id_1`) REFERENCES `permissions` (`id`);
+
+ALTER TABLE `groups_perm_relation` ADD FOREIGN KEY (`perm_id_2`) REFERENCES `permissions` (`id`);
+
+ALTER TABLE `groups_perm_relation` ADD FOREIGN KEY (`perm_id_3`) REFERENCES `permissions` (`id`);
