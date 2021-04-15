83310164234a51c419376356c9eb1aa49523aa53
Protiuc Ovidiu <oprotiuc@computervoice.ro>
2021-04-15 17:16:02 +0300

    dwq

---
+++ test/sign_up.py
@@ -0,0 +1,15 @@
+
+class Person:
+  def __init__(full_name, user_name, email, password, pass_confirm):
+    self.full_name = full_name
+    self.user_name = user_name
+    self.email = email
+    self.password = password
+    self.pass_confirm = pass_confirm
+
+  def check_pass(password, pass_confirm):
+    if pass_confirm != password:
+        return false
+  def insert():
+    query = ""
+  
